54ce45dc72c4260d1a7b0044fb096aae22ce529b
Sudeep Reddy <sudeep04@gmail.com>
2019-06-04 15:45:10 +0530

    Andriod app working with firebase

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ A new Flutter project for Be AI
 ## Login Screen
 
 ![application screenshot](flutter_01.png "BeAI login screen")
+
+
+## After Login
+
+![application screenshot](flutter_02.png "Home Page")
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:be_ai/services/authentication.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.auth, this.userId, this.onSignedOut})
+      : super(key: key);
+
+  final BaseAuth auth;
+  final VoidCallback onSignedOut;
+  final String userId;
+
+  @override
+  State<StatefulWidget> createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  bool _isEmailVerified = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _checkEmailVerification();
+  }
+
+  void _checkEmailVerification() async {
+    _isEmailVerified = await widget.auth.isEmailVerified();
+    if (!_isEmailVerified) {
+      _showVerifyEmailDialog();
+    }
+  }
+
+  void _resentVerifyEmail(){
+    widget.auth.sendEmailVerification();
+    _showVerifyEmailSentDialog();
+  }
+
+  void _showVerifyEmailDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Verify your account"),
+          content: new Text("Please verify account in the link sent to email"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("Resent link"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _resentVerifyEmail();
+              },
+            ),
+            new FlatButton(
+              child: new Text("Dismiss"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showVerifyEmailSentDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Verify your account"),
+          content: new Text("Link to verify account has been sent to your email"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("Dismiss"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+
+  _signOut() async {
+    try {
+      await widget.auth.signOut();
+      widget.onSignedOut();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+
+  Widget _showNewsFeed() {
+
+      return Center(child: Text("Welcome. The following are the articles",
+        textAlign: TextAlign.start,
+        style: TextStyle(fontSize: 20.0),));
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('BeAI App'),
+          actions: <Widget>[
+            new FlatButton(
+                child: new Text('Logout',
+                    style: new TextStyle(fontSize: 17.0, color: Colors.white)),
+                onPressed: _signOut)
+          ],
+        ),
+        body: _showNewsFeed(),
+    );
+  }
+}
